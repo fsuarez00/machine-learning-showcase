1cc578a17fe09bb9b101333182732105415971a4
Franklin Suarez <fsuarez@investoranalytics.com>
2016-11-28 15:56:51 -0500

    initial commit of project

---
+++ src/main/java/com/fsuarez/Launcher.java
@@ -0,0 +1,25 @@
+package com.fsuarez;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+/**
+ * @author fsuarez
+ */
+public class Launcher {
+
+    public static void main(String... args) throws ParseException {
+
+        Options options = new Options();
+        options.addOption("a", true, "Algorithm to run");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+
+        String algorithm = cmd.getOptionValue("a");
+    }
+
+}
