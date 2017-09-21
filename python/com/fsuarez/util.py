b43ef0b7405baed1a2e35222c1967e63e4d05def
Franklin Suarez <fsuarez@investoranalytics.com>
2017-09-21 14:58:33 -0400

    completed logistic regression showcase with regularization and one vs all

--- python/com/fsuarez/util.py
+++ python/com/fsuarez/util.py
@@ -6,8 +6,8 @@ def map_features(x1, x2):
     out = None
     for i in range(1, degree+1):
         for j in range(0, i+1):
-            x_1 = np.reshape(np.power(x1, i - j), (47, 1))
-            x_2 = np.reshape(np.power(x2, j), (47, 1))
+            x_1 = np.reshape(np.power(x1, i - j), (x1.size, 1))
+            x_2 = np.reshape(np.power(x2, j), (x2.size, 1))
             if i is 1 and j is 0:
                 out = x_1 * x_2
             else:
